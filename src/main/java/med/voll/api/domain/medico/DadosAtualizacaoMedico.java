544e4e813c218ef5a5f6d97b66e78a8418190a31
Jonathan Philipe <sicrano3000@gmail.com>
2023-06-22 15:08:40 -0300

    Criando tratamento de erros

--- src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedico.java
@@ -1,9 +1,9 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosAtualizacaoMedico(
         @NotNull
